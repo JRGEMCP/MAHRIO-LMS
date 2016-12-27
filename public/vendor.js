727ae332797765821cfc81123d5f8de6b5a739ac
Jesus Guerrero <rocha.jesus@hotmail.com>
2016-12-26 16:27:27 -0800

    Adding dist

---
+++ public/vendor.js
@@ -0,0 +1,12 @@
+webpackJsonp([2,3],[
+/* 0 */
+/***/ function(module, exports) {
+
+	/**
+	 * Created by mahrio on 11/26/16.
+	 */
+	"use strict";
+
+/***/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvdmVuZG9yLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSIsImZpbGUiOiJ2ZW5kb3IuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIENyZWF0ZWQgYnkgbWFocmlvIG9uIDExLzI2LzE2LlxuICovXG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9zcmMvdmVuZG9yLmpzIl0sInNvdXJjZVJvb3QiOiIifQ==
