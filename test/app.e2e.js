53d24b0ef42b737cdb4087794d06dd53db937725
Erik <emcp@whichdegree.co>
2017-01-13 19:37:33 -0800

    Tests (#9)
    * Adding some clarity on E2E steps
    
    * Attempting to make ids easier to find
    
    * Test is properly failing as expected with current code

--- test/app.e2e.js
+++ test/app.e2e.js
@@ -6,4 +6,10 @@ describe('app', () => {
   it('has a title', () => {
     expect(browser.getTitle()).toEqual('Mahr.io - Learning Management System');
   });
+
+  it('should not be logged in', () => {
+    const logoutHeader = element(by.id('5-logout-header'));
+    console.log(logoutHeader.getId());
+    expect(logoutHeader.isDisplayed()).toBe(false);
+  });
 });
