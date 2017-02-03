f8679f284af4a03a7ec3abb7e2bc75fba2384229
EMCP <emcp@whichdegree.co>
2017-02-02 20:52:10 -0800

    Adding testcase from master branch

--- test/app.e2e.js
+++ test/app.e2e.js
@@ -6,4 +6,8 @@ describe('app', () => {
   it('has a title', () => {
     expect(browser.getTitle()).toEqual('Mahr.io - Learning Management System');
   });
+
+  it('should not be logged in', () => {
+    expect(element(by.id('5-logout-header')).isPresent()).toEqual(false);
+  });
 });
