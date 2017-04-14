9032b08ecd486af2c6b683a612bbbf80e31ca5fb
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-04-14 00:42:12 -0700

    Update core.module.js

--- src/app/_core/src/core.module.js
+++ src/app/_core/src/core.module.js
@@ -16,6 +16,8 @@ import { Featured } from './components/featured/featured.component';
 import { List } from './components/network/list/list.component';
 import { MiddleAlign } from './components/middle-align/middle-align.component';
 import { Milestone } from './components/milestone/milestone.component';
+import { MilestoneList } from './components/milestone-list/milestone-list.component';
+import { MilestonePanel } from './components/milestone-panel/milestone-panel.component';
 import { NetworkShow } from './components/network/show/show.component';
 import { Objectives } from './components/objectives/objectives.component';
 import { RecentProgress } from './components/progress/progress.component';
@@ -23,6 +25,8 @@ import { NgbdStarRating } from './components/rating/rating.component';
 import { Rewards } from './components/rewards/rewards.component';
 import { TopNavigation } from './components/top-navigation/top-navigation.component';
 import { SelectComp } from './components/select/select.component';
+
+import { MilestoneService } from './services/milestone.service';
 
 @NgModule({
   imports: [
@@ -52,6 +56,9 @@ import { SelectComp } from './components/select/select.component';
     Featured,
     Milestone,
   ],
+  providers: [
+    MilestoneService  
+  ],
   exports: [
     Conversation,
     CardsHeader,
@@ -72,6 +79,7 @@ import { SelectComp } from './components/select/select.component';
     Jumbotron,
     Featured,
     Milestone,
+    MilestoneService  
   ]
 })
 
